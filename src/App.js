a497c321b1baeb78ba840d9671c6bcfd0e510aea
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-06-15 12:06:16 +0200

    Add Emotion for styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5rem;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 function App() {
-  return <h1>Breaking bad</h1>;
+  const requestAPI = () => {
+    console.log('Sending request...');
+  };
+
+  return (
+    <Wrapper>
+      <Button onClick={requestAPI}>Get phrase</Button>
+    </Wrapper>
+  );
 }
 
 export default App;
